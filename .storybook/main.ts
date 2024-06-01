d41247357e2d6acc50f0d200c4c5db5afa7491bd
Denis Laluev <laluevdenis@yandex.ru>
2024-06-01 14:15:15 +0300

    feat: add src path alias

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -13,6 +15,30 @@ const config: StorybookConfig = {
     name: "@storybook/react-vite",
     options: {},
   },
-  staticDirs: ["../public"],
+  viteFinal: (config) => {
+    config.define = { "process.env": {} };
+
+    if (!config.plugins) config.plugins = [];
+
+    config.plugins.push(
+      tsconfigPaths({
+        // My tsconfig.json isn't simply in viteConfig.root,
+        // so I've passed an explicit path to it:
+        projects: [path.resolve(path.dirname(__dirname), "tsconfig.json")],
+      })
+    );
+
+    return {
+      ...config,
+      resolve: {
+        alias: [
+          {
+            find: "@",
+            replacement: path.resolve(__dirname, "./src/"),
+          },
+        ],
+      },
+    };
+  },
 };
 export default config;
