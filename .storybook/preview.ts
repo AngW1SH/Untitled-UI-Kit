5f6aa01ea043e225e522af58a9d6211d26ce3e82
Denis Laluev <laluevdenis@yandex.ru>
2024-06-05 19:33:32 +0300

    fix: use expanded controls for storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -5,6 +5,7 @@ import "../src/app/globals.css";
 const preview: Preview = {
   parameters: {
     controls: {
+      expanded: true,
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
